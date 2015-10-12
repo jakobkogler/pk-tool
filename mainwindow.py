a816cbcd07206d89839822ee1038d1cbcf75f0d7
Jakob Kogler <jakob.kogler@gmail.com>
2015-10-12 23:11:33 +0200

    initial commit

---
+++ mainwindow.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mainwindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.5
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(734, 550)
+        self.centralWidget = QtWidgets.QWidget(MainWindow)
+        self.centralWidget.setObjectName("centralWidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralWidget)
+        self.verticalLayout.setContentsMargins(11, 11, 11, 11)
+        self.verticalLayout.setSpacing(6)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setContentsMargins(11, 11, 11, 11)
+        self.horizontalLayout.setSpacing(6)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.label = QtWidgets.QLabel(self.centralWidget)
+        self.label.setObjectName("label")
+        self.horizontalLayout.addWidget(self.label)
+        self.files_combobox = QtWidgets.QComboBox(self.centralWidget)
+        self.files_combobox.setObjectName("files_combobox")
+        self.horizontalLayout.addWidget(self.files_combobox)
+        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.table_widget = QtWidgets.QTableWidget(self.centralWidget)
+        self.table_widget.setObjectName("table_widget")
+        self.table_widget.setColumnCount(0)
+        self.table_widget.setRowCount(0)
+        self.table_widget.verticalHeader().setVisible(False)
+        self.verticalLayout.addWidget(self.table_widget)
+        MainWindow.setCentralWidget(self.centralWidget)
+        self.menuBar = QtWidgets.QMenuBar(MainWindow)
+        self.menuBar.setGeometry(QtCore.QRect(0, 0, 734, 27))
+        self.menuBar.setObjectName("menuBar")
+        MainWindow.setMenuBar(self.menuBar)
+        self.mainToolBar = QtWidgets.QToolBar(MainWindow)
+        self.mainToolBar.setObjectName("mainToolBar")
+        MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.mainToolBar)
+        self.statusBar = QtWidgets.QStatusBar(MainWindow)
+        self.statusBar.setObjectName("statusBar")
+        MainWindow.setStatusBar(self.statusBar)
+        self.toolBar = QtWidgets.QToolBar(MainWindow)
+        self.toolBar.setObjectName("toolBar")
+        MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar)
+        self.action_export = QtWidgets.QAction(MainWindow)
+        self.action_export.setObjectName("action_export")
+        self.toolBar.addAction(self.action_export)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "PK Übungstool"))
+        self.label.setText(_translate("MainWindow", "Gruppe: "))
+        self.toolBar.setWindowTitle(_translate("MainWindow", "toolBar"))
+        self.action_export.setText(_translate("MainWindow", "Export"))
+        self.action_export.setShortcut(_translate("MainWindow", "Ctrl+S"))
+
---
+++ pk_tool.py
@@ -0,0 +1,116 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, \
+    QTableWidgetItem, QMessageBox, QPlainTextEdit, QShortcut, QCheckBox, QWidget, QHBoxLayout
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtCore import pyqtSlot
+from mainwindow import Ui_MainWindow
+import os
+import re
+import io
+from collections import namedtuple
+from _datetime import datetime
+
+
+Student = namedtuple('Student', 'name matrikelnr email')
+
+
+class PkToolMainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+
+        self.find_files()
+        self.current_group_idx = 0
+        self.checked_list = []
+        self.open_file(0)
+
+        self.files_combobox.currentIndexChanged.connect(self.open_file)
+        self.action_export.triggered.connect(lambda: self.write_file(savefile=False))
+        self.table_widget.cellChanged.connect(lambda: self.write_file(savefile=True))
+
+    def find_files(self):
+        r = re.compile('185\.A79 Programmkonstruktion \(VU 6,0\) 2015W_Übungsanmeldung \(.* Gruppen\)_(.*?)_Überblick.txt')
+        matches = [r.search(f) for f in os.listdir('.')]
+        self.name_files = [m.group(0) for m in matches if m]
+        shortcuts = [m.group(1) for m in matches if m]
+        self.files_combobox.addItems(shortcuts)
+
+    def open_file(self, index):
+        if self.current_group_idx != index:
+            really = QMessageBox.question(self, 'Öffnen',
+                            'Möchten Sie die Gruppe wechseln?\n Alle Änderungenen werden gelöscht!',
+                            QMessageBox.Yes | QMessageBox.No | QMessageBox.Cancel)
+            if really != QMessageBox.Yes:
+                self.files_combobox.setCurrentIndex(self.current_group_idx)
+                return
+
+        self.current_group_idx = index
+
+        with open(self.name_files[index], 'r') as f:
+            students = []
+            r = re.compile('\s+✔\s+(\D+)\s(\d+)\s(.*)\s?')
+            for line in f:
+                m = r.search(line)
+                if m:
+                    students.append(Student(*(m.group(i) for i in range(1, 4))))
+
+        self.table_widget.clear()
+        labels = 'Name Matrikelnr. Anwesend Adhoc Kommentar'.split()
+        self.table_widget.setRowCount(len(students))
+        self.table_widget.setColumnCount(len(labels))
+        self.table_widget.setHorizontalHeaderLabels(labels)
+
+        self.checked_list = []
+
+        for idx, student in enumerate(sorted(students)):
+            name_item = QTableWidgetItem(student.name)
+            name_item.setFlags(name_item.flags() & ~QtCore.Qt.ItemIsEditable)
+            self.table_widget.setItem(idx, 0, name_item)
+
+            matrikelnr_item = QTableWidgetItem(student.matrikelnr)
+            matrikelnr_item.setFlags(matrikelnr_item.flags() & ~QtCore.Qt.ItemIsEditable)
+            matrikelnr_item.setTextAlignment(QtCore.Qt.AlignCenter)
+            self.table_widget.setItem(idx, 1, matrikelnr_item)
+
+            check_item = QWidget()
+            chk_bx = QCheckBox()
+            chk_bx.setCheckState(QtCore.Qt.Unchecked)
+            chk_bx.stateChanged.connect(lambda: self.write_file(savefile=True))
+            lay_out = QHBoxLayout(check_item)
+            lay_out.addWidget(chk_bx)
+            lay_out.setAlignment(QtCore.Qt.AlignCenter)
+            lay_out.setContentsMargins(0,0,0,0)
+            check_item.setLayout(lay_out)
+            self.checked_list.append(chk_bx)
+            self.table_widget.setCellWidget(idx, 2, check_item)
+
+            self.table_widget.setItem(idx, 3, QTableWidgetItem())
+
+            self.table_widget.setItem(idx, 4, QTableWidgetItem())
+
+        self.table_widget.resizeColumnsToContents()
+
+    def write_file(self, savefile=True):
+        if savefile:
+            now = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
+            file_name = 'Saves/{}_{}.csv'.format(self.files_combobox.currentText(), now)
+        else:
+            file_name = '{}.csv'.format(self.files_combobox.currentText())
+
+        with io.open(file_name, 'w', encoding='utf-8', newline='') as f:
+            f.write('MatrNr;Gruppe;Kontrolle;Kommentar\n')
+            for idx in range(self.table_widget.rowCount()):
+                f.write('{};{};{};{}% {}\n'.format(
+                    self.table_widget.item(idx, 1).text(),
+                    self.files_combobox.currentText(),
+                    'an' if self.checked_list[idx].isChecked() else 'ab',
+                    self.table_widget.item(idx, 3).text() or '0',
+                    self.table_widget.item(idx, 4).text()
+                ))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = PkToolMainWindow()
+    window.show()
+    sys.exit(app.exec_())
