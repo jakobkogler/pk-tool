35085e19daf4c22b8f73480238391886139093a5
Jakob Kogler <jakob.kogler@gmail.com>
2015-11-09 10:55:11 +0100

    Read group data from pk-repo files

--- pk-tool.py
+++ pk-tool.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QFileDialog, QCheckBox, QWidget, QHBoxLayout, QMessageBox
 from PyQt5 import QtCore
+from PyQt5.QtCore import QSettings
 from mainwindow import Ui_MainWindow
 import os
 import re
@@ -9,6 +10,7 @@ from collections import namedtuple
 from _datetime import datetime
 
 
+Group = namedtuple('Group', 'day name type students')
 Student = namedtuple('Student', 'name matrikelnr email')
 
 
@@ -20,33 +22,96 @@ class PkToolMainWindow(QMainWindow, Ui_MainWindow):
         self.name_files = []
         self.history_files = []
         self.history_index = None
-        self.find_files()
-        self.current_group_idx = 0
-        self.write_lock = False
-        if self.name_files:
-            self.open_file(0)
-        else:
-            QMessageBox().about(self, 'Keine Teilnehmerlisten gefunden',
-                                '\n'.join(['Um das Programm verwenden zu können, müssen Sie zuerst die Teilnehmerlisten',
-                                           'von Tuwel downloaden und in dieses Verzeichnis kopieren. Die findet man unter',
-                                           '"Programmkonstruktion - Anmeldungen - Übungsanmeldung (Normale Gruppen) - Teilnehmer/innen". ',
-                                           'Dort bei den jeweiligen Gruppen die .txt (z.B. 185.A79...Überblick.txt) nehmen.']))
 
-        self.files_combobox.currentIndexChanged.connect(self.open_file)
-        self.action_export.triggered.connect(lambda: self.write_file(savefile=False))
-        self.table_widget.cellChanged.connect(lambda: self.write_file(savefile=True))
-        self.console.returnPressed.connect(self.execute_console)
-        self.action_new.triggered.connect(lambda: self.open_file(self.files_combobox.currentIndex(), new=True))
-        self.action_undo.triggered.connect(self.history_undo)
-        self.action_redo.triggered.connect(self.history_redo)
-        self.action_add_student.triggered.connect(self.add_row)
+        self.settings = QSettings('settings.ini', QSettings.IniFormat)
+        pk_repo_path = self.settings.value('Path/pk_repo', '')
+        self.line_edit_repo_path.setText(pk_repo_path)
+        if pk_repo_path:
+            self.read_group_files()
+
+        # self.find_files()
+        # self.current_group_idx = 0
+        # self.write_lock = False
+        # if self.name_files:
+        #     self.open_file(0)
+        # else:
+        #     QMessageBox().about(self, 'Keine Teilnehmerlisten gefunden',
+        #                         '\n'.join(['Um das Programm verwenden zu können, müssen Sie zuerst die Teilnehmerlisten',
+        #                                    'von Tuwel downloaden und in dieses Verzeichnis kopieren. Die findet man unter',
+        #                                    '"Programmkonstruktion - Anmeldungen - Übungsanmeldung (Normale Gruppen) - Teilnehmer/innen". ',
+        #                                    'Dort bei den jeweiligen Gruppen die .txt (z.B. 185.A79...Überblick.txt) nehmen.']))
+        #
+        # self.group_combobox.currentIndexChanged.connect(self.open_file)
+        # self.action_export.triggered.connect(lambda: self.write_file(savefile=False))
+        # self.table_widget.cellChanged.connect(lambda: self.write_file(savefile=True))
+        # self.console.returnPressed.connect(self.execute_console)
+        # self.action_new.triggered.connect(lambda: self.open_file(self.group_combobox.currentIndex(), new=True))
+        # self.action_undo.triggered.connect(self.history_undo)
+        # self.action_redo.triggered.connect(self.history_redo)
+        # self.action_add_student.triggered.connect(self.add_row)
+        self.button_select_repo_path.clicked.connect(self.select_repo_path)
+        self.group_type_combobox.currentIndexChanged.connect(self.fill_group_names_combobox)
+        self.group_day_combobox.currentIndexChanged.connect(self.fill_group_names_combobox)
+
+    def read_group_files(self):
+        path_template = self.line_edit_repo_path.text() + '/Anwesenheiten/Anmeldung/groups_{group_type}.txt'
+        self.groups = []
+
+        for group_type in 'fortgeschritten', 'normal':
+            with open(path_template.format(group_type=group_type), 'r', encoding='utf-8') as f:
+                group_name_regex = re.compile('(mo|di|mi|do|fr)\d{2}\w')
+                student_regex = re.compile('\s+[+✔]\s+(\D+)\s(\d+)\s(.*)\s?')
+
+                students = []
+                group_name = ''
+
+                for line in f:
+                    group_name_match = group_name_regex.search(line)
+                    students_match = student_regex.search(line)
+
+                    if group_name_match:
+                        if students:
+                            self.groups.append(Group(group_name[:2], group_name[2:], group_type, students))
+                            students = []
+                        group_name = group_name_match.group(0)
+                    elif students_match:
+                        students.append(Student(*(students_match.group(i) for i in range(1, 4))))
+
+            if students:
+                self.groups.append(Group(group_name[:2], group_name[2:], group_type, students))
+
+        self.group_type_combobox.addItems('Alle Normal Fortgeschritten'.split())
+        self.group_day_combobox.addItems('mo di mi do fr'.split())
+        self.fill_group_names_combobox()
+
+    def fill_group_names_combobox(self):
+        type_index = self.group_type_combobox.currentIndex()
+        allowed_types = []
+        if type_index != 1:
+            allowed_types.append('fortgeschritten')
+        if type_index != 2:
+            allowed_types.append('normal')
+
+        group_names = [group.name for group in self.groups
+                  if group.type in allowed_types and group.day == self.group_day_combobox.currentText()]
+        self.group_combobox.clear()
+        self.group_combobox.addItems(group_names)
+
+    def load_group_data(self):
+        pass
+
+    def select_repo_path(self):
+        pk_repo_path = QFileDialog.getExistingDirectory(self, 'Pfad zum PK-Repository', self.line_edit_repo_path.text(), QFileDialog.ShowDirsOnly)
+        if pk_repo_path:
+            self.settings.setValue('Path/pk_repo', pk_repo_path)
+            self.line_edit_repo_path.setText(pk_repo_path)
 
     def find_files(self):
         r = re.compile('185\.A79 Programmkonstruktion .*_(.*?)_Überblick.txt')
         matches = [r.search(f) for f in os.listdir('.')]
         self.name_files = [m.group(0) for m in matches if m]
         shortcuts = [m.group(1) for m in matches if m]
-        self.files_combobox.addItems(shortcuts)
+        self.group_combobox.addItems(shortcuts)
 
     def attendance_changed(self):
         self.write_file(savefile=True)
@@ -77,7 +142,7 @@ class PkToolMainWindow(QMainWindow, Ui_MainWindow):
         self.table_widget.setItem(idx, 1, matrikelnr_item)
 
         if student:
-            group_item = QTableWidgetItem(self.files_combobox.currentText())
+            group_item = QTableWidgetItem(self.group_combobox.currentText())
             group_item.setFlags(matrikelnr_item.flags() & ~QtCore.Qt.ItemIsEditable)
         else:
             group_item = QTableWidgetItem()
@@ -154,9 +219,9 @@ class PkToolMainWindow(QMainWindow, Ui_MainWindow):
             self.table_widget.item(idx, 3).setData(0, int(self.get_checkbox(idx).isChecked()))
 
     def get_savefiles(self):
-        current_group = self.files_combobox.currentText()
+        current_group = self.group_combobox.currentText()
         if os.path.exists('Saves'):
-            self.history_files = sorted(f for f in os.listdir('Saves') if f.startswith(current_group))
+            self.history_files = sorted(set(f for f in os.listdir('Saves') if f.startswith(current_group)))
 
     def history_undo(self):
         if self.history_index:
@@ -202,7 +267,7 @@ class PkToolMainWindow(QMainWindow, Ui_MainWindow):
         if self.write_lock:
             return
 
-        group_name = self.files_combobox.itemText(self.current_group_idx)
+        group_name = self.group_combobox.itemText(self.current_group_idx)
         if savefile:
             now = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
             file_name = 'Saves/{}_{}.csv'.format(group_name, now)
