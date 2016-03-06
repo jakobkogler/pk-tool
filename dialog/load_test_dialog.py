bee1fbd8ffe47bfdc2fbfb2bd02ae6094899ca4d
Jakob Kogler <jakob.kogler@gmail.com>
2016-03-06 12:11:52 +0100

    Change load test dialog

--- dialog/load_test_dialog.py
+++ dialog/load_test_dialog.py
@@ -1,4 +1,3 @@
-import re
 import os
 from PyQt5.QtWidgets import QDialog, QFileDialog
 from ui.load_test_dialog import Ui_LoadTestDialog
@@ -18,25 +17,22 @@ class LoadTestDialog(QDialog, Ui_LoadTestDialog):
         self.setupUi(self)
 
         self.settings = settings
+        self.path_to_folders = '{}/Anwesenheiten/Tests/'.format(self.settings.repo_path)
+        self.test_folders = sorted(os.listdir(self.path_to_folders))
+        self.folder_combobox.addItems(self.test_folders)
 
-        self.button_application_file.pressed.connect(self.load_application_list)
-        self.button_folder_path.pressed.connect(self.load_folder_path)
+        self.button_application_file.pressed.connect(self.find_application_list)
+        self.button_load_application_list.pressed.connect(self.load_application_list)
 
-    def load_application_list(self):
+    def find_application_list(self):
         path, _ = QFileDialog.getOpenFileName(self, 'Pfad zur Anmeldungsliste',
                                               self.line_edit_application_file.text(), 'Textdatei (*.txt)')
         self.line_edit_application_file.setText(path)
 
-        pattern = re.compile(r'(Test\d+)')
-        match = pattern.search(path)
-        if match:
-            test_name = match.group(1)
-            path_to_folder = '{}/Anwesenheiten/Tests/{}'.format(self.settings.repo_path, test_name)
-            if os.path.isdir(path_to_folder):
-                self.line_edit_folder_path.setText(path_to_folder)
+        for idx, folder in enumerate(self.test_folders):
+            if folder in path:
+                self.folder_combobox.setCurrentIndex(idx)
+                break
 
-    def load_folder_path(self):
-        path = QFileDialog.getExistingDirectory(self, 'Pfad zum Ordner des Tests',
-                                                self.line_edit_folder_path.text(), QFileDialog.ShowDirsOnly)
-        self.line_edit_folder_path.setText(path)
-
+    def load_application_list(self):
+        pass
--- ui/load_test_dialog.py
+++ ui/load_test_dialog.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'ui/load_test_dialog.ui'
+# Form implementation generated from reading ui file 'load_test_dialog.ui'
 #
 # Created by: PyQt5 UI code generator 5.5.1
 #
@@ -14,6 +14,10 @@ class Ui_LoadTestDialog(object):
         LoadTestDialog.resize(593, 226)
         self.gridLayout = QtWidgets.QGridLayout(LoadTestDialog)
         self.gridLayout.setObjectName("gridLayout")
+        self.folder_combobox = QtWidgets.QComboBox(LoadTestDialog)
+        self.folder_combobox.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.folder_combobox.setObjectName("folder_combobox")
+        self.gridLayout.addWidget(self.folder_combobox, 3, 1, 1, 1)
         self.line_edit_application_file = QtWidgets.QLineEdit(LoadTestDialog)
         self.line_edit_application_file.setObjectName("line_edit_application_file")
         self.gridLayout.addWidget(self.line_edit_application_file, 2, 1, 1, 1)
@@ -23,37 +27,27 @@ class Ui_LoadTestDialog(object):
         self.label_folder_path = QtWidgets.QLabel(LoadTestDialog)
         self.label_folder_path.setObjectName("label_folder_path")
         self.gridLayout.addWidget(self.label_folder_path, 3, 0, 1, 1)
-        self.line_edit_folder_path = QtWidgets.QLineEdit(LoadTestDialog)
-        self.line_edit_folder_path.setObjectName("line_edit_folder_path")
-        self.gridLayout.addWidget(self.line_edit_folder_path, 3, 1, 1, 1)
-        self.button_folder_path = QtWidgets.QPushButton(LoadTestDialog)
-        self.button_folder_path.setObjectName("button_folder_path")
-        self.gridLayout.addWidget(self.button_folder_path, 3, 2, 1, 1)
+        self.button_application_file = QtWidgets.QPushButton(LoadTestDialog)
+        self.button_application_file.setObjectName("button_application_file")
+        self.gridLayout.addWidget(self.button_application_file, 2, 2, 1, 1)
         self.label_description = QtWidgets.QLabel(LoadTestDialog)
         self.label_description.setWordWrap(True)
         self.label_description.setObjectName("label_description")
         self.gridLayout.addWidget(self.label_description, 1, 0, 1, 3)
-        self.button_application_file = QtWidgets.QPushButton(LoadTestDialog)
-        self.button_application_file.setObjectName("button_application_file")
-        self.gridLayout.addWidget(self.button_application_file, 2, 2, 1, 1)
-        self.horizontalLayout = QtWidgets.QHBoxLayout()
-        self.horizontalLayout.setObjectName("horizontalLayout")
-        self.pushButton = QtWidgets.QPushButton(LoadTestDialog)
-        self.pushButton.setObjectName("pushButton")
-        self.horizontalLayout.addWidget(self.pushButton)
-        self.gridLayout.addLayout(self.horizontalLayout, 4, 0, 1, 3)
+        self.button_load_application_list = QtWidgets.QPushButton(LoadTestDialog)
+        self.button_load_application_list.setObjectName("button_load_application_list")
+        self.gridLayout.addWidget(self.button_load_application_list, 4, 0, 1, 3)
 
         self.retranslateUi(LoadTestDialog)
         QtCore.QMetaObject.connectSlotsByName(LoadTestDialog)
 
     def retranslateUi(self, LoadTestDialog):
         _translate = QtCore.QCoreApplication.translate
-        LoadTestDialog.setWindowTitle(_translate("LoadTestDialog", "Test-Modus"))
+        LoadTestDialog.setWindowTitle(_translate("LoadTestDialog", "Laden der Anmeldungsliste für einen Test"))
         self.label_application_file.setText(_translate("LoadTestDialog", "Pfad zur Anmeldungsliste: "))
-        self.label_folder_path.setText(_translate("LoadTestDialog", "Pfad zum Ordner des Tests:"))
-        self.button_folder_path.setText(_translate("LoadTestDialog", "Auswählen"))
-        self.label_description.setText(_translate("LoadTestDialog", "Zum Bearbeiten der Anwesenheit benötigt das Programm eine aktuelle Anmeldungsliste. Diese kann unter TUWEL - Kurs Programmkonstruktion - Anmeldung zu Test X - Teilnehmer als \".txt\"-Datei heruntergeladen werden. \n"
-"Ebenfalls wird der Pfad zum Ordner benötigt, in dem alle Anwesenheitslisten gespeichert werden sollen. "))
+        self.label_folder_path.setText(_translate("LoadTestDialog", "Test-Name:"))
         self.button_application_file.setText(_translate("LoadTestDialog", "Auswählen"))
-        self.pushButton.setText(_translate("LoadTestDialog", "Wechsle zum Test-Modus"))
+        self.label_description.setText(_translate("LoadTestDialog", "Zum Bearbeiten der Anwesenheit benötigt das Programm eine aktuelle Anmeldungsliste. Diese kann unter TUWEL - Kurs Programmkonstruktion - Anmeldung zu Test X - Teilnehmer als \".txt\"-Datei heruntergeladen werden. \n"
+"Ebenfalls wird der Testname (Testnummer) benötigt. "))
+        self.button_load_application_list.setText(_translate("LoadTestDialog", "Lade die Anmeldungsliste"))
 
